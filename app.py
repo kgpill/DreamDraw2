28f3a4a8aa8a19e4a0a810a0fe737ca14f7f49a3
kgpill <101847318+kgpill@users.noreply.github.com>
2024-12-04 05:36:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ STABILITY_API_KEY = os.getenv("API_KEY")
 # MySQL 설정
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PORT'] = os.getenv("MYSQL_PORT")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
